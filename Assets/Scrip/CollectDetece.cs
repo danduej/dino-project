2409707cb22c8b477053717110c4374c7237f489
Fookzzaza <fook555666@gmail.com>
2025-03-29 05:16:39 +0700

    Add CollisionFx , shake camera and FadeOut

--- Assets/Scrip/CollectDetece.cs
+++ Assets/Scrip/CollectDetece.cs
@@ -6,12 +6,23 @@ public class CollectDeti : MonoBehaviour
 {
     [SerializeField] GameObject thePlayer;
     [SerializeField] GameObject playerAnim;
+    [SerializeField] AudioSource collisionFX;
+    [SerializeField] GameObject mainCam;
+    [SerializeField] GameObject fadeout;
     void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(CollisionEnd());
+    }
+    IEnumerator CollisionEnd()
+    {
+        collisionFX.Play();
         thePlayer.GetComponent<PlayerMovement>().enabled = false;
         playerAnim.GetComponent<Animator>().Play("Stumble Backwards");
+        mainCam.GetComponent<Animator>().Play("ColilsionCam");
+        yield return new WaitForSeconds(3);
+        fadeout.SetActive(true);
     }
 
-
 }
+
 
